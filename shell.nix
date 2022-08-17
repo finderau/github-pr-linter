f64946bc36111b9b70b892b1c6efb7949befdb10
Jason O'Conal <jason@finder.com>
2022-08-17 13:45:59 +0930

    feat: lints PR title and body
    We've got our own style for git commits at Finder
    (https://github.com/finderau/engineering-standards/blob/master/general/2018-05-02-git-commit-standards.md)
    and we want to ensure that PRs to our repos can be automatically linted
    against this standard.
    
    This commit sets up a simple action that checks the PR for:
    
    1. The appropriate title format
    2. A Jira reference (not a link) in the body
    3. Some description in the body of the PR (hopefully containing
       information on the _why_ behind the change)
    4. A blank line between the description and the Jira reference

---
+++ shell.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  lib = import <nixpkgs/lib>;
+  buildNodejs = pkgs.callPackage <nixpkgs/pkgs/development/web/nodeJs/nodeJs.nix> { python = pkgs.python310; };
+
+  NPM_CONFIG_PREFIX = toString ./npm_config_prefix;
+
+in pkgs.mkShell {
+  packages = with pkgs; [
+    nodePackages.npm
+    nodejs-18_x
+  ];
+
+  inherit NPM_CONFIG_PREFIX;
+
+  shellHook = ''
+    export PATH="${NPM_CONFIG_PREFIX}/bin:$PATH"
+    export NODE_EXTRA_CA_CERTS="$HOME/dev/netskope/config/chainCA.crt"
+  '';
+}
